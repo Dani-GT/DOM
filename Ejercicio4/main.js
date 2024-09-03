20a8b3e9dd7d57557d0d7586cc01c9369881bce1
Dani-GT <dani_spain87@hotmail.com>
2024-09-03 12:34:47 +0200

    exercise 4 done

--- Ejercicio4/main.js
+++ Ejercicio4/main.js
@@ -1,11 +1,27 @@
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
 // evento click que ejecute un console log con la información del evento del click
 
+const btn = document.getElementById('btnToClick');
+btn.addEventListener('click', function(event) {
+    console.log(event);
+});
+
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+const focusInput = document.querySelector('input.focus');
+focusInput.addEventListener('focus', (event) => {
+    console.log('Valor del input en focus:', event.target.value);
+});
 
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
 
-// Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
+const valueInput = document.querySelector('input.value');
+valueInput.addEventListener('input', (event) => {
+    console.log('Valor del input en input:', event.target.value);
+});
+
+
+// 1.4 Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
 // que imprima cada uno de los albums.
 
 const albums = [
@@ -15,3 +31,13 @@ const albums = [
   "Painkiller",
   "Iron Fist",
 ];
+
+const ulAlbums = document.createElement("ul");
+
+for (const single of albums) {
+    const liSingles = document.createElement("li");
+    liSingles.textContent = single;
+    ulAlbums.appendChild(liSingles);
+}
+
+document.body.appendChild(ulAlbums);
